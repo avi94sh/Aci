43e70f2a56bce96e0fb1efd6f6d2a7d72452d657
avi94sh <139676735+avi94sh@users.noreply.github.com>
2023-07-27 19:46:38 +0530

    Add files via upload

---
+++ assigmen01_dsa.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h> // header file containing rand() function
+
+char avi(int num)  // main condition which differentiates 0-9 and A-F characters
+{
+    if( 0<= num  && num<=9)
+    {
+        return  '0' + num;   
+    }
+
+                         else if( num <=15)
+                         {
+                           return 'A' + (num -10 ); 
+                                                   }
+
+             else{
+             printf("Enter a valid number");
+                                            }
+}
+
+    
+int main()
+{
+
+    
+    
+
+    int num;
+     
+              num = rand() % 15;
+     
+                                printf("%d\n",num);
+
+
+                    char temp = avi(num);
+    printf("The Randomly Generated Character is :%c" ,temp);
+
+
+    return 0;
+}
